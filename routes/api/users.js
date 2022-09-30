3917d1204d33f195bf59426d12fae02b4422849d
Harshal141 <harshalmukundapatil@gmail.com>
2022-09-30 13:18:08 +0530

    save user to database

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,28 +2,60 @@ const express = require('express');
 const router = express.Router();
 // express validator https://express-validator.github.io/docs/
 const { check, validationResult } = require('express-validator');
+// geting the table/collection from the database
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @route  post api/users
 // @desc   register user
 // @access Public
 
 router.post('/',[
-    // check name is not empty
+    // check validaition
     check('name', 'Name is required').not().isEmpty(),
-    // check email is valid
     check('email', 'Please include a valid email').isEmail(),
-    // check password is at least 6 characters
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-],(req,res)=>{
-    // content we send in req using a middleware
-    // console.log(req.body);
+],
+async (req,res)=>{
+    // content we send in req using a middleware console.log(req.body);
     // check for errors
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         // send 400 status and json object with errors
         return res.status(400).json({errors: errors.array()});
     }
-    res.send('user router');
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if(user){ // return error of type as above if user exists
+            return res.status(400).json({errors: [{msg: 'User already exists'}]});
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({ // create new user instance
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10); // generate salt
+        user.password = await bcrypt.hash(password, salt); // hash password
+
+        await user.save(); // save user to database
+
+        res.send('user registered');
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }   
 })
 
 module.exports = router;
