209d4bd27d91ab3da9a57d02b4e368f0e8da5f19
Harshal141 <harshalmukundapatil@gmail.com>
2022-10-04 21:34:18 +0530

    like and dislike added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -94,4 +94,29 @@ router.delete('/:id',auth,async (req,res)=>{
     }
 });
 
+// @route  PUT api/posts/like/:id
+// @desc   like a post
+// @access private
+
+router.put('/like/:id',auth,async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        // check if the post has already been liked
+        if(post.likes.filter(like=>like.user.toString() === req.user.id).length > 0){
+            // dislike
+            const removeIndex = post.likes.map(like=>like.user.toString()).indexOf(req.user.id);
+            post.likes.splice(removeIndex,1);
+            await post.save();
+            return res.json(post.likes);
+        }
+        post.likes.unshift({user:req.user.id});
+        await post.save();
+        res.json(post.likes);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 module.exports = router;
