711ccafdb89e55786d3572e9277cf0d708b19448
Harshal141 <harshalmukundapatil@gmail.com>
2022-10-04 19:42:43 +0530

    get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,18 @@ router.post('/',[ auth,[
     }
 });
 
+// @route  GET api/posts
+// @desc   get all posts
+// @access private
+
+router.get('/',auth,async (req,res)=>{
+    try{
+        const posts = await Post.find().sort({date:-1});
+        res.json(posts);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
