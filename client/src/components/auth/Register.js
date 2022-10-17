aaf7cee5fce8bec456cd33f59b2a475dd65d3e88
Harshal141 <harshalmukundapatil@gmail.com>
2022-10-17 10:27:15 +0530

    registration linking front to back

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,9 +5,10 @@ import {connect} from 'react-redux';
 import {setAlert} from '../../actions/alert';
 import PropTypes from 'prop-types';
 import Alert from '../layout/Alert';
+import {register} from '../../actions/auth';
 
 
-const Register = ({ setAlert}) => {
+const Register = ({ setAlert,register}) => {
   const [formData,setFormData] = useState({
     name: '',
     email: '',
@@ -23,7 +24,7 @@ const Register = ({ setAlert}) => {
     if(password !== password2){
       setAlert('Passwords do not match','danger');
     }else{
-      console.log('Success')
+      register({name,email,password});
       // const newUser = {
       //   name,
       //   email,
@@ -93,7 +94,8 @@ const Register = ({ setAlert}) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 }
 
-export default connect(null,{setAlert})(Register);
+export default connect(null,{setAlert,register})(Register);
