e8c09f769b0584df50dbfdb3eeab7cf346db09f3
Harshal141 <harshalmukundapatil@gmail.com>
2022-11-04 18:42:02 +0530

    buggy save navbar logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import {Link} from 'react-router-dom'
+import {connect } from 'react-redux';
+import { ReactPropTypes } from 'react';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({auth:{isAuthenticated,loading},logout}) => {
+  const authLinks = (
+    <li className="nav-item">
+          <Link className="nav-link" to="/register"><a onClick={logout} href="#!">
+        <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span></a></Link>
+    </li>
+    // <ul>
+    //   <li>
+    //     <a onClick={logout} href="#!">
+    //     <i className="fas fa-sign-out-alt"></i>{' '}
+    //       <span className="hide-sm">Logout</span></a>
+    //   </li>
+    // </ul>
+  )
+  const guestLinks = (
+    <>
+    <li className="nav-item">
+      <Link className="nav-link" to="!#">Developers</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/register">Register</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/login">Login</Link>
+    </li>
+    </>
+  )
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
@@ -14,15 +44,7 @@ const Navbar = () => {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="!#">Developers</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">Register</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
+        {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
       </ul>
     </div>
   </div>
@@ -30,4 +52,13 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+Navbar.ReactPropTypes = {
+  logout: ReactPropTypes.func.isRequired,
+  auth: ReactPropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps,{logout})(Navbar);
